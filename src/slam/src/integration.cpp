d8fa60098e1420b641da2ee0f7a3b1b1d8edf630
Parth Patel <parth.pmech@gmail.com>
2024-07-26 19:23:03 -0400

    Remove Eigen As Library and Change to Thread

--- src/slam/src/integration.cpp
+++ src/slam/src/integration.cpp
@@ -5,6 +5,9 @@
 #include <tf2/LinearMath/Matrix3x3.h>
 
 #include <boost/bind.hpp>
+#include <opencv2/opencv.hpp>
+#include <thread>
+#include <mutex>
 
 #include "sensor_msgs/LaserScan.h"
 #include "slam/process_sensor_data.h"
@@ -18,49 +21,64 @@ constexpr std::size_t lidar_data_points_counts = 360;
 const pose_t lidar_to_robot_transformation{0.0F, 0.0F, 0.0F};
 /////////////////////////// Mapping Variables /////////////////////////////
 constexpr float grid_size = 10.0f;
+////////////////////////// Thread Specific Variables ////////////////////////
+#define NUM_ROBOTS 1
+
+typedef struct
+{
+    std::unique_ptr<lidar_data> lidarObject;
+    std::unique_ptr<robot_odometry> robotOdomObject;
+    std::unique_ptr<map_environment> robotMap;
+    pose_t robot_pose;
+    std::vector<pose_t> all_pose_robot_global_frame;
+    cv::Mat slam_map_image;
+    ros::Subscriber encoder_sub;
+    ros::Subscriber lidar_sub;
+    std::mutex data_mutex; // Mutex to protect data access
+
+} Robot;
 
 void lidarCallback(const sensor_msgs::LaserScan::ConstPtr &msg,
-                   std::unique_ptr<robot_odometry> &robotOdomObject,
-                   std::unique_ptr<lidar_data> &lidarObject,
-                   std::unique_ptr<map_environment> &robotMap,
-                   pose_t &current_pose,
-                   std::vector<pose_t> &all_pose_robot_global_frame,
-                   cv::Mat &slam_map_image)
+                   Robot &robot,
+                   const int idx)
 {
-    if (!lidarObject)
+    std::lock_guard<std::mutex> lock(robot.data_mutex);
+
+    if (!robot.lidarObject)
     {
         // Initialize lidarScan object using the first message
-        if (!robotOdomObject)
+        if (!robot.robotOdomObject)
         {
             return;
         }
-        lidarObject = std::make_unique<lidar_data>(
+        robot.lidarObject = std::make_unique<lidar_data>(
             msg->ranges.size(),
             value_range_t{.min = msg->range_min, .max = msg->range_max},
             0.650F,
             lidar_to_robot_transformation,
             0.0F,
             1000.0F);
-        robotMap = std::make_unique<map_environment>(1000.0, grid_size);
+        robot.robotMap = std::make_unique<map_environment>(1000.0, grid_size);
         ROS_INFO("Lidar Initialization Success!");
     }
 
     std::vector<float> scans{msg->ranges};
-    const auto obstacles_robot_frame = lidarObject->process_lidar_scan_to_robot_frame(scans, current_pose);
-    auto currentMap_robot_origin_frame = robotMap->updateMap_robot_origin_frame(obstacles_robot_frame, current_pose);
+    const auto obstacles_robot_frame = robot.lidarObject->process_lidar_scan_to_robot_frame(scans, robot.robot_pose);
+    auto currentMap_robot_origin_frame = robot.robotMap->updateMap_robot_origin_frame(obstacles_robot_frame, robot.robot_pose);
 
-    all_pose_robot_global_frame.push_back(current_pose);
+    robot.all_pose_robot_global_frame.push_back(robot.robot_pose);
 
-    Viz::gridToImage(slam_map_image, currentMap_robot_origin_frame);
-    Viz::plotGraphPoints(slam_map_image, all_pose_robot_global_frame, Viz::red, slam_map_image.cols, robotMap);
-    cv::imshow("Lidar Map", slam_map_image);
+    Viz::gridToImage(robot.slam_map_image, currentMap_robot_origin_frame);
+    Viz::plotGraphPoints(robot.slam_map_image, robot.all_pose_robot_global_frame, Viz::red, robot.slam_map_image.cols, robot.robotMap);
+    cv::imshow("Lidar Map " + std::to_string(idx), robot.slam_map_image);
     cv::waitKey(10);
 }
 
 void encoderCallback(const nav_msgs::Odometry::ConstPtr &msg, // Position is in Meters
-                     pose_t &currentPose,
-                     std::unique_ptr<robot_odometry> &robotOdomObject)
+                     Robot &robot)
 {
+    std::lock_guard<std::mutex> lock(robot.data_mutex);
+
     auto quat = msg->pose.pose.orientation;
     tf2::Quaternion tf_quat(quat.x, quat.y, quat.z, quat.w);
     tf2::Matrix3x3 mat(tf_quat);
@@ -69,9 +87,9 @@ void encoderCallback(const nav_msgs::Odometry::ConstPtr &msg, // Position is in 
 
     // ROS_INFO("Encoder Callback");
     // Initialize lidarScan object using the first message
-    if (!robotOdomObject)
+    if (!robot.robotOdomObject)
     {
-        robotOdomObject = std::make_unique<robot_odometry>(
+        robot.robotOdomObject = std::make_unique<robot_odometry>(
             robot_width,
             msg->pose.pose.position.x * 1000.0F,
             msg->pose.pose.position.y * 1000.0F,
@@ -79,7 +97,7 @@ void encoderCallback(const nav_msgs::Odometry::ConstPtr &msg, // Position is in 
         ROS_INFO("Encoder Initialization Success!");
     }
 
-    currentPose = robotOdomObject->m_update_pose(
+    robot.robot_pose = robot.robotOdomObject->m_update_pose(
         msg->pose.pose.position.x * 1000.0F,
         msg->pose.pose.position.y * 1000.0F,
         yaw);
@@ -89,32 +107,35 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "slam_integration");
     ros::NodeHandle nh;
-    std::unique_ptr<lidar_data> lidarObject;
-    std::unique_ptr<robot_odometry> robotOdomObject;
-    std::unique_ptr<map_environment> robotMap;
-    pose_t robot_pose;
-    std::vector<pose_t> all_pose_robot_global_frame;
-    cv::Mat slam_map_image;
-    cv::namedWindow("Lidar Map", cv::WINDOW_AUTOSIZE);
 
-    ros::Subscriber encoder_sub = nh.subscribe<nav_msgs::Odometry>("odom", 1000,
-                                                                   boost::bind(
-                                                                       encoderCallback,
-                                                                       _1,
-                                                                       boost::ref(robot_pose),
-                                                                       boost::ref(robotOdomObject)));
-    ros::Subscriber lidar_sub = nh.subscribe<sensor_msgs::LaserScan>("scan", 1000,
-                                                                     boost::bind(
-                                                                         lidarCallback,
-                                                                         _1,
-                                                                         boost::ref(robotOdomObject),
-                                                                         boost::ref(lidarObject),
-                                                                         boost::ref(robotMap),
-                                                                         boost::ref(robot_pose),
-                                                                         boost::ref(all_pose_robot_global_frame),
-                                                                         boost::ref(slam_map_image)));
-    ros::spin();
-    cv::destroyWindow("Lidar Map");
+    std::vector<std::thread> robot_threads;
+    std::vector<std::unique_ptr<Robot>> robots;
 
+    for (int i = 0; i < NUM_ROBOTS; ++i)
+    {
+        robots.push_back(std::make_unique<Robot>());
+    }
+
+    for (int i = 0; i < NUM_ROBOTS; ++i)
+    {
+        robot_threads.emplace_back([&nh, &robots, i]()
+                                   {
+            auto &robotData = *robots[i];
+            robotData.encoder_sub = nh.subscribe<nav_msgs::Odometry>( "/odom", 1000,
+                                                                      boost::bind(encoderCallback, _1, boost::ref(robotData)));
+            robotData.lidar_sub = nh.subscribe<sensor_msgs::LaserScan>("/scan", 1000,
+                                                                       boost::bind(lidarCallback, _1, boost::ref(robotData), i));
+
+            ros::spin();
+            cv::destroyWindow("Lidar Map " + std::to_string(i)); });
+    }
+
+    for (auto &thread : robot_threads)
+    {
+        if (thread.joinable())
+        {
+            thread.join();
+        }
+    }
     return 0;
 }
